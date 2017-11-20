59783670ff3a97c5c283f291871def41de257f1a
a053629318b <a053629318b@S09-PC08>
2017-11-20 09:01:37 +0100

    add

---
+++ public_html/js/app/carrito/1/add.js
@@ -0,0 +1,46 @@
+'use strict';
+
+moduloCarrito.controller('CarritoAdd2Controller',
+        ['$scope', '$routeParams', 'serverCallService', '$location', 'sessionService', 'constantService','objectService',
+            function ($scope, $routeParams, serverCallService, $location, sessionService, constantService,objectService) {
+                $scope.ob = "carrito";
+                $scope.op = "add";
+                $scope.profile = 1;
+                //---
+                $scope.status = null;
+                $scope.debugging = constantService.debugging();
+                $scope.url = $scope.ob + '/' + $scope.profile + '/' + $scope.op;
+                //---
+                $scope.bean = {};
+                $scope.bean.cantidad = 0;
+                $scope.bean.id = $routeParams.id;
+                //---
+                $scope.objectService = objectService;
+                //---
+                
+                $scope.add = function () {
+                    serverCallService.add($scope.ob, $scope.id, $scope.cantidad).then(function (response) {
+                        if (response.status == 200) {
+                            if (response.data.status == 200) {
+                                $scope.bean.id = response.data.json;
+                                $scope.status = "El registro con id=" + $scope.id + " y cantidad="+ $scope.cantidad +"se ha añadido al carro.";
+                           
+                            } else {
+                                $scope.status = "Error en la recepción de datos del servidor 2";
+                            }
+                        } else {
+                            $scope.status = "Error en la recepción de datos del servidor 3";
+                        }
+                    }).catch(function (data) {
+                        $scope.status = "Error en la recepción de datos del servidor 4";
+                    });
+                }
+                $scope.back = function () {
+                    window.history.back();
+                };
+                $scope.close = function () {
+                    $location.path('/home');
+                };
+}]);
+
+
--- public_html/js/system/config/routes.js
+++ public_html/js/system/config/routes.js
@@ -103,6 +103,8 @@ trolleyes.config(['$routeProvider', function ($routeProvider) {
          $routeProvider.when('/carrito/1/plist/:page?/:rpp?', {templateUrl: 'js/app/carrito/1/plist.html', controller: 'CarritoPList1Controller', resolve: {auth: authenticationAdministratorPromise}}); 
          $routeProvider.when('/carrito/1/remove/:id', {templateUrl: 'js/app/carrito/1/remove.html', controller: 'CarritoRemove1Controller', resolve: {auth: authenticationAdministratorPromise}});
          $routeProvider.when('/carrito/1/empty/:page?/:rpp?', {templateUrl: 'js/app/carrito/1/empty.html', controller: 'CarritoEmpty1Controller', resolve: {auth: authenticationAdministratorPromise}});
+         $routeProvider.when('/carrito/1/add/:id', {templateUrl: 'js/app/carrito/1/add.html', controller: 'CarritoAdd2Controller', resolve: {auth: authenticationClientPromise}});
+
         //-------Carrito cliente-------------------
          $routeProvider.when('/carrito/2/plist/:page?/:rpp?', {templateUrl: 'js/app/carrito/2/plist.html', controller: 'CarritoPList2Controller', resolve: {auth: authenticationClientPromise}}); 
          $routeProvider.when('/carrito/2/remove/:id', {templateUrl: 'js/app/carrito/2/remove.html', controller: 'CarritoRemove2Controller', resolve: {auth: authenticationClientPromise}});
